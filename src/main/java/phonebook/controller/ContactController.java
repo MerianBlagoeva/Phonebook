b098566050cd23f9e0aca959fc2e6404f15b649f
Merian <drundrun00@gmail.com>
2023-04-16 06:05:52 +0300

    Edit and Delete don't work

--- src/main/java/phonebook/controller/ContactController.java
+++ src/main/java/phonebook/controller/ContactController.java
@@ -2,12 +2,14 @@ package phonebook.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import phonebook.entity.Contact;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ContactController {
@@ -24,4 +26,36 @@ public class ContactController {
         return modelAndView;
     }
 
+    @PostMapping("/")
+    public String add(Contact contact) {
+        this.contacts.add(contact);
+        return "redirect:/";
+    }
+
+    @GetMapping("edit{name}/")
+    public ModelAndView editForm(@PathVariable String name, ModelAndView modelAndView) {
+        Optional<Contact> optionalContact = this.contacts.stream().filter(c -> c.getName().equals(name)).findFirst();
+        Contact contact = optionalContact.orElseThrow(() -> new IllegalArgumentException("No contact found with name " + name));
+
+        modelAndView.setViewName("edit");
+        modelAndView.addObject("contact", contact);
+
+        return modelAndView;
+    }
+
+    @PutMapping("/edit{name}")
+    public String edit (@PathVariable String name, Contact contact) {
+        Optional<Contact> optionalContact = this.contacts.stream().filter(c -> c.getName().equals(name)).findFirst();
+        Contact contact1 = optionalContact.orElseThrow(() -> new IllegalArgumentException("No contact found with name " + name));
+        contact1.setName(contact.getName());
+        contact1.setNumber(contact.getNumber());
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/delete{name}")
+    public String delete(@PathVariable String name) {
+        this.contacts.removeIf(contact -> contact.getName().equals(name));
+
+        return "redirect:/";
+    }
 }
--- src/main/java/phonebook/entity/Contact.java
+++ src/main/java/phonebook/entity/Contact.java
@@ -1,27 +1,16 @@
 package phonebook.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Contact {
     private String name;
     private String number;
 
-    public Contact(String name, String number) {
-        this.name = name;
-        this.number = number;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
 }
